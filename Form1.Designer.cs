32a7729785f4a62392d7cf1c6cb863ca8341c40f
Will <34465137+i81be4udid@users.noreply.github.com>
2017-12-11 22:15:03 -0500

    Add files via upload
    This was my version of the Car Class, from Learning C# from Pearson, and Tony Gaddis

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Car_Proj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnBrake = new System.Windows.Forms.Button();
+            this.btnAcc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "MODEL";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "MAKE";
+            // 
+            // txtModel
+            // 
+            this.txtModel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtModel.Location = new System.Drawing.Point(426, 96);
+            this.txtModel.Margin = new System.Windows.Forms.Padding(4);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(148, 22);
+            this.txtModel.TabIndex = 13;
+            // 
+            // txtMake
+            // 
+            this.txtMake.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMake.Location = new System.Drawing.Point(426, 8);
+            this.txtMake.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(148, 22);
+            this.txtMake.TabIndex = 12;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(32, 206);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 164);
+            this.listBox1.TabIndex = 11;
+            // 
+            // btnBrake
+            // 
+            this.btnBrake.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrake.Location = new System.Drawing.Point(453, 334);
+            this.btnBrake.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrake.Name = "btnBrake";
+            this.btnBrake.Size = new System.Drawing.Size(112, 28);
+            this.btnBrake.TabIndex = 10;
+            this.btnBrake.Text = "Brake";
+            this.btnBrake.UseVisualStyleBackColor = true;
+            this.btnBrake.Click += new System.EventHandler(this.BtnBrake_Click);
+            // 
+            // btnAcc
+            // 
+            this.btnAcc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcc.Location = new System.Drawing.Point(453, 264);
+            this.btnAcc.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAcc.Name = "btnAcc";
+            this.btnAcc.Size = new System.Drawing.Size(112, 28);
+            this.btnAcc.TabIndex = 9;
+            this.btnAcc.Text = "Accelerate";
+            this.btnAcc.UseVisualStyleBackColor = true;
+            this.btnAcc.Click += new System.EventHandler(this.BtnAcc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 398);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtMake);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnBrake);
+            this.Controls.Add(this.btnAcc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnBrake;
+        private System.Windows.Forms.Button btnAcc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Car_Proj
+{
+    public partial class Form1 : Form
+    {
+        private Car myCar;
+
+        public Form1()
+        {
+            myCar = new Car();
+
+            InitializeComponent();
+        }
+
+        private void GetCarData(Car car)
+        {
+            try
+            {
+                myCar.Make = txtMake.Text;
+
+                myCar.Year = int.Parse(txtModel.Text);
+
+                myCar.Speed = 0;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Concat("Must enter a valid make and year model for the car. ", ex.Message, "\r\n", ex.StackTrace));
+            }
+        }
+
+        private void BtnAcc_Click(object sender, EventArgs e)
+        {
+            GetCarData(myCar);
+            myCar.AccSpeed(5);
+            MessageBox.Show(" Your car is a " + myCar.Year + myCar.Make + " and it is     traveling " + myCar.Speed + " mph. ");
+        }
+
+        private void BtnBrake_Click(object sender, EventArgs e)
+        {
+            GetCarData(myCar);
+            myCar.DecSpeed(5);
+            MessageBox.Show(" Your car is a " + myCar.Year + myCar.Make + " and it is     traveling " + myCar.Speed + " mph. ");
+        }
+    }
+}
